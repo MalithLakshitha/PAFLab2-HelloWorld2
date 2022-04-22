f8e700bfc106e2c5ee918c1d8f77c7efce6a030d
MalithLakshitha <it20226596@my.sliit.lk>
2022-04-22 20:37:06 +0530

    commiting for changes

--- src/com/sliit/main/Main.java
+++ src/com/sliit/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Lab Sheet 02. Pratical 2.1");
+		System.out.println("#GOHOMEGOTA2022");
 	}
 
 }
